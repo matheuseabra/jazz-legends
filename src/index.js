e4d540107cd6e3a29c002f951ee316fed09e5b37
Matheus Seabra <matheusvieiracoelho@gmail.com>
2018-09-27 08:53:33 -0300

    form validation, localStorage, and obj props added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 //Select elements needed
 const playerForm = document.querySelector("#addPlayerForm");
 const playersList = document.querySelector(".players");
-const players = [];
+const players = JSON.parse(localStorage.getItem("players", players)) || [];
+
+populateList(players, playersList);
 
 //Listen for the submit and adds the player
 playerForm.addEventListener("submit", function(e) {
@@ -9,25 +11,37 @@ playerForm.addEventListener("submit", function(e) {
   e.preventDefault();
 
   //Grab the value from the jazz player name
-  const playerName = document.querySelector("#playerName").value;
+  const name = document.querySelector("#playerName").value;
+  const instrument = document.querySelector("#playerInstrument").value;
+  const genre = document.querySelector("#playerGenre").value;
 
-  //Create the obj with respective value
-  const player = {
-    name: playerName
-  };
+  if (name && instrument && genre) {
+    //Create the obj with respective value
+    const player = {
+      name,
+      instrument,
+      genre
+    };
 
-  //add player to player's array
-  players.push(player);
-  populateList(players, playersList);
-  this.reset();
-  // console.table(players);
+    //add player to player's array
+    players.push(player);
+    populateList(players, playersList);
+    localStorage.setItem("players", JSON.stringify(players));
+    this.reset();
+  } else {
+    alert("Hey, seems like some fields are missing...");
+  }
 });
 
 // Function to populateList
 function populateList(items = [], itemsList) {
   itemsList.innerHTML = items
     .map(function(item, i) {
-      return "<li>" + item.name + "</li>";
+      return `<li>
+                <h3>${item.name}</h3>
+                <p>Intrument: ${item.instrument}</p>
+                <p>Genre: ${item.genre}</p>    
+              </li>`;
     })
     .join("");
 }
